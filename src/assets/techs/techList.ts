70fb92e4fd2a1538bd4f6659e516be6ed50b050e
igorb <igorbabin2002@gmail.com>
2026-01-27 16:46:31 +0500

    Added links to backend techs

--- src/assets/techs/techList.ts
+++ src/assets/techs/techList.ts
@@ -10,56 +10,69 @@ import aws from "@/assets/techs/aws.png";
 import otel from "@/assets/techs/otel.png";
 import immediate from "@/assets/techs/immediateplatform.png";
 import blazor from "@/assets/techs/blazor.png";
+type TechData = { name: string, image: ImageMetadata, link: string };
 
-export const techList = {
+export const techList : { back: TechData[] } = {
     back: [
         {
             name: 'ASP.NET Core',
-            image: aspnet
+            image: aspnet,
+            link: 'https://dotnet.microsoft.com/ru-ru/apps/aspnet'
         },
         {
             name: 'Immediate.Apis',
-            image: immediate
+            image: immediate,
+            link: 'https://github.com/ImmediatePlatform/Immediate.Apis/'
         },
         {
             name: 'Blazor',
-            image: blazor
+            image: blazor,
+            link: 'https://dotnet.microsoft.com/ru-ru/apps/aspnet/web-apps/blazor'
         },
         {
             name: 'EFCore',
-            image: ef
+            image: ef,
+            link: 'https://learn.microsoft.com/ru-ru/ef/core/'
         },
         {
             name: 'npgsql',
-            image: npgsql
+            image: npgsql,
+            link: 'https://www.npgsql.org/efcore/'
         },
         {
             name: 'Clickhouse.Driver',
-            image: clickhouse
+            image: clickhouse,
+            link: 'https://clickhouse.com/docs/integrations/csharp'
         },
         {
             name: 'StackExchange.Redis',
-            image: redis
+            image: redis,
+            link: 'https://stackexchange.github.io/StackExchange.Redis/'
         },
         {
             name: 'Serilog',
-            image: serilog
+            image: serilog,
+            link: 'https://serilog.net/'
         },
         {
             name: 'AWSSDK.S3',
-            image: aws
+            image: aws,
+            link: 'https://github.com/aws/aws-sdk-net/'
         },
         {
             name: 'xUnit',
-            image: xunit
+            image: xunit,
+            link: 'https://xunit.net/'
         },
         {
             name: 'testcontainers',
-            image: testcontainers
+            image: testcontainers,
+            link: 'https://testcontainers.com/?language=dotnet'
         },
         {
             name: 'OpenTelemetry',
-            image: otel
+            image: otel,
+            link: 'https://opentelemetry.io/docs/languages/dotnet/'
         },
     ]
 } as const;
